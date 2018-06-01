3a0b1d36f0c5c01388e93b4e03417142997c29b1
LMC <2_minchul@naver.com>
2018-06-02 05:05:59 +0900

    update comment

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
  *
  * Use of this source code is governed by a WTFPL license that can be
  * found in the LICENSE file.
+ *
+ * C++ BattleShip Project
+ * 2018-06-02
+ * 20163137 이민철
  */
 
 #include "BattleShipApp.h"
