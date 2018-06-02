bbef643e29b557beb35f6539deccb8a68bc18ea4
LMC <2_minchul@naver.com>
2018-06-02 18:22:10 +0900

    GameManager 필드 & 메서드 정의

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "GameManager.h"
+
+void GameManager::AddTurn() {
+  turn_++;
+}
+
+void GameManager::AttackByInput() {
+
+}
+
+int GameManager::GetGameMode() {
+  return game_mode_;
+}
+
+int GameManager::GetShips() {
+  return 0;
+}
+
+int GameManager::GetTurn() {
+  return turn_;
+}
+
+void GameManager::Init() {
+
+}
+
+bool GameManager::IsDeadShip() {
+  return false;
+}
+
+void GameManager::Render() {
+
+}
+
+void GameManager::SetGameMode(int game_mode) {
+  game_mode_ = game_mode;
+}
+
+GameManager *GameManager::GetInstance() { // Singleton instance
+  if (instance == nullptr) instance = new GameManager();
+  return instance;
+}
+
+GameManager *GameManager::instance = nullptr;
--- src/GameManager.h
+++ src/GameManager.h
@@ -6,9 +6,35 @@
 #define BATTLESHIP_GAMEMANAGER_H_
 
 #include "types.h"
+#include "Player.h"
 
 class GameManager {
+ private:
+  GameManager() {};
+  GameManager(const GameManager &other);
+  ~GameManager() {};
 
+  static GameManager *instance;
+
+ public:
+  static GameManager *GetInstance();
+
+  void AddTurn();
+  void AttackByInput();
+  int GetGameMode();
+  int GetShips();
+  int GetTurn();
+  void Init();
+  bool IsDeadShip();
+  void Render();
+  void SetGameMode(int game_mode);
+
+ protected:
+  Player *attacker_;
+  Player *defender_;
+  int game_mode_ = PLAY_INPUT;
+  int ships_;
+  int turn_ = 0;
 };
 
 #endif // BATTLESHIP_GAMEMANAGER_H_
