db44a3bba4e67198b1662045db561faab37378a8
LMC <2_minchul@naver.com>
2018-06-02 11:26:32 +0900

    Map Pane을 정사각형으로 출력

--- src/BattleShipMap.cpp
+++ src/BattleShipMap.cpp
@@ -11,7 +11,7 @@
 
 #include "BattleShipMap.h"
 
-BattleShipMap::BattleShipMap() : Pane(4, 4, MAP_SIZE + 3, MAP_SIZE + 2) {
+BattleShipMap::BattleShipMap() : Pane(4, 4, MAP_SIZE + 3 + 8, MAP_SIZE + 2) {
   for (int i = 0; i < MAP_SIZE; i++) {
     for (int j = 0; j < MAP_SIZE; ++j) {
       // 맵 데이터 초기화
@@ -22,11 +22,11 @@ BattleShipMap::BattleShipMap() : Pane(4, 4, MAP_SIZE + 3, MAP_SIZE + 2) {
   // 칸 구별 이름
   for (int i = 0; i < MAP_SIZE; ++i) {
     mvprintw(i + 1 + y_, x_ - 1, "%c", 'A' + i);
-    mvprintw(y_ + height_, x_ + 2 + i, "%d", 1 + i);
+    mvprintw(y_ + height_, x_ + 2 + 2 * i, "%d", 1 + i);
   }
 
   // 타이틀
-  mvwprintw(window_, 0, 3, "< MAP >");
+  mvwprintw(window_, 0, (width_ / 2) - 3, "< MAP >");
 }
 
 BattleShipMap::~BattleShipMap() {}
@@ -35,7 +35,7 @@ void BattleShipMap::Draw() {
   wattron(window_, COLOR_PAIR(COLOR_MAP));
   for (int i = 0; i < MAP_SIZE; ++i) {
     for (int j = 0; j < MAP_SIZE; ++j) {
-      mvwprintw(window_, i + 1, j + 2, "%c", map_data_[i][j]);
+      mvwprintw(window_, i + 1, (2 * j) + 2, "%c", map_data_[i][j]);
     }
   }
   wattroff(window_, COLOR_PAIR(COLOR_MAP));
